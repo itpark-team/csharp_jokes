034514f595485c244f4586bc41cf88ad1c70e40f
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-05-16 18:25:50 +0300

    change connections string

--- RestWebApplication/Models/RestDatabaseContext.cs
+++ RestWebApplication/Models/RestDatabaseContext.cs
@@ -26,8 +26,10 @@ namespace RestWebApplication.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=u1236834.plsk.regruhosting.ru;Database=u1236834_jokes;User Id=u1236834_admin;Password=QWE123$%^;Trusted_Connection=false;");
+                optionsBuilder.UseSqlServer("Server=u1236834.plsk.regruhosting.ru;Database=u1236834_jokes;User Id=u1236834_groups;Password=reallyStrongPwd123;Trusted_Connection=false;");
             }
+
+           
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
